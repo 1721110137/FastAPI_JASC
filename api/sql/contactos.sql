4cb88a25ffd019289c9448189c24319898688970
José Alfredo Sorcia Cuellar <1721110137@utectulancingo.edu.mx>
2022-09-27 21:27:37 +0000

    Script tabla contactos y registros

---
+++ api/sql/contactos.sql
@@ -0,0 +1,10 @@
+CREATE TABLE contactos(
+    nombre VARCHAR(50),
+    email VARCHAR(50),
+    telefono VARCHAR(50)
+);
+
+INSERT INTO contactos (nombre, email, telefono) VALUES
+("Diana", "diana@email.com", "123456789"),
+("Luis", "luis@email.com", "123456789"),
+("Sandra", "sandra@email.com", "123456789");
